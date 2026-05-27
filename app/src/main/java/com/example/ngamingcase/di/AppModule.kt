ea1668fa15948405989427ecf34cad0817d97a76
batuhanbaki <42751544+batuhanbaki@users.noreply.github.com>
2026-05-28 02:53:31 +0300

    added ssl pinning

--- app/src/main/java/com/example/ngamingcase/di/AppModule.kt
+++ app/src/main/java/com/example/ngamingcase/di/AppModule.kt
@@ -8,6 +8,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,10 +18,20 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-    @Provides @Singleton
+    @Provides
+    @Singleton
     fun provideApi(): PostsApiService {
         val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
-        val client = OkHttpClient.Builder().addInterceptor(logger).build()
+        val pinner = CertificatePinner.Builder()
+            .add(
+                "jsonplaceholder.typicode.com",
+                "sha256/e89QAFJvkB7Tn3QGfsNheN8fgTxZgLECjap1xSq628w=",
+                "sha256/kIdp6NNEd8wsugYyyIYFsi1ylMCED3hZbSR8ZFsa/A4="
+            )
+            .build()
+        val client = OkHttpClient.Builder()
+            .certificatePinner(pinner)
+            .addInterceptor(logger).build()
         return Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com/")
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
@@ -31,6 +42,7 @@ object NetworkModule {
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-    @Binds @Singleton
+    @Binds
+    @Singleton
     abstract fun bindRepo(impl: PostRepositoryImpl): PostRepository
 }
